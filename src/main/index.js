47729e29a1d975e322b0be1bb89b8db7e67f4221
Mazees <madaadha21@gmail.com>
2026-07-02 23:05:43 +0700

    refactor: disable opening DevTools on main window ready event

--- src/main/index.js
+++ src/main/index.js
@@ -53,7 +53,7 @@ function createWindow() {
 
   mainWindow.on('ready-to-show', () => {
     mainWindow.show()
-    mainWindow.webContents.openDevTools()
+    // mainWindow.webContents.openDevTools()
   })
 
   mainWindow.webContents.setWindowOpenHandler((details) => {
